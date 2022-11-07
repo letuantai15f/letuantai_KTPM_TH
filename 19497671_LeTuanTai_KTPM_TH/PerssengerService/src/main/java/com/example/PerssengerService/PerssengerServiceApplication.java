4a76b26afd66c1b04e3c4963d471790ef2dbf67c
TaiLee <letuantai15f@gmail.com>
2022-11-07 13:53:28 +0700

    finish dicovery service

--- 19497671_LeTuanTai_KTPM_TH/PerssengerService/src/main/java/com/example/PerssengerService/PerssengerServiceApplication.java
+++ 19497671_LeTuanTai_KTPM_TH/PerssengerService/src/main/java/com/example/PerssengerService/PerssengerServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.PerssengerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableRetry
+@EnableEurekaClient
 public class PerssengerServiceApplication {
 
 	public static void main(String[] args) {
