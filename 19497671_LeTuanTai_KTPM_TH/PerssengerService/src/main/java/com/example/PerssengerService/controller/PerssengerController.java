628d6418a5a6a929b1ec2cf3448da3576f91f971
TaiLee <letuantai15f@gmail.com>
2022-11-07 13:37:26 +0700

    ‘finish cache for passenger service

--- 19497671_LeTuanTai_KTPM_TH/PerssengerService/src/main/java/com/example/PerssengerService/controller/PerssengerController.java
+++ 19497671_LeTuanTai_KTPM_TH/PerssengerService/src/main/java/com/example/PerssengerService/controller/PerssengerController.java
@@ -1,12 +1,14 @@
 package com.example.PerssengerService.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.PerssengerService.entity.Billing;
 import com.example.PerssengerService.entity.Perssenger;
@@ -17,11 +19,14 @@ import com.example.PerssengerService.service.PerssengerService;
 public class PerssengerController {
 	@Autowired
 	private PerssengerService service;
+	@Autowired
+	private RedisTemplate template;
 	
 	@PostMapping("/create")
 	public Perssenger createPerssenger(@RequestBody Perssenger perssenger) {
-		Perssenger perssenger2=service.createPerssenger(perssenger);
-		return perssenger2;
+		perssenger=service.createPerssenger(perssenger);
+		template.opsForHash().put("product", perssenger.getPerssengerId(), perssenger);
+		return perssenger;
 	}
 	@GetMapping("/perssenger/bill/{id}")
 	public String getPerssengerId(@PathVariable("id") int perssengerID) {
@@ -31,6 +36,12 @@ public class PerssengerController {
 	@GetMapping("")
 	public Perssenger getPerssenger() {
 		Perssenger perssenger=service.getPerssenger();
+		return perssenger;
+	}
+	@GetMapping("/{id}")
+	public Perssenger getPerssenger2(@PathVariable("id") int id) {
+		Perssenger perssenger=(Perssenger) template.opsForHash().get("product", id);
+		System.out.println(perssenger);
 		return perssenger;
 	}
 }
